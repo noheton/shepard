e60d4395f795a375787cdb6dda17fdd646406ffd
Tobias Haase <tobias.haase@dlr.de>
2022-06-24 08:57:06 +0200

    make pylint happy

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ TOKEN_FILE = "token.txt"
 
 if __name__ == "__main__":
 
-    token = ""
+    TOKEN = ""
     with open(TOKEN_FILE, "r", encoding="UTF-8") as token_file:
-        token = token_file.read()
+        TOKEN = token_file.read()
 
     project_name = input("Project: ")
     if project_name not in PROJECTS:
         print(f"Project ID {project_name} is not known")
         sys.exit(1)
 
-    sys.exit(create_release(GITLAB_INSTANCE, token, PROJECTS[project_name]))
+    sys.exit(create_release(GITLAB_INSTANCE, TOKEN, PROJECTS[project_name]))
