339fde1c7725f7451e49ce370af5f81d86330db2
Tobias Haase <tobias.haase@dlr.de>
2022-09-14 14:12:47 +0200

    Replace JSP error page

--- src/main/java/de/dlr/shepard/exceptions/ShepardExceptionMapper.java
+++ src/main/java/de/dlr/shepard/exceptions/ShepardExceptionMapper.java
@@ -1,15 +1,19 @@
 package de.dlr.shepard.exceptions;
 
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class ShepardExceptionMapper implements ExceptionMapper<ShepardException> {
+public class ShepardExceptionMapper implements ExceptionMapper<Exception> {
 
 	@Override
-	public Response toResponse(ShepardException exception) {
-		var status = exception.getStatusCode();
+	public Response toResponse(Exception exception) {
+		int status = Status.INTERNAL_SERVER_ERROR.getStatusCode();
+		if (exception instanceof ShepardException shepardException) {
+			status = shepardException.getStatusCode();
+		}
 
 		return Response.status(status)
 				.entity(new ApiError(status, exception.getClass().getSimpleName(), exception.getMessage())).build();
--- src/main/webapp/error.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" isErrorPage="true" import="java.io.*" contentType="application/json"%>{
-  "Status": <%=response.getStatus() %>,
-  "Exception": "<%if(exception != null) {%><%=exception.getCause()%><%}%>",
-  "Message": "<%if(exception != null) {%><%=exception.getMessage()%><%}%>"
-}
