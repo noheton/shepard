206cfff6de77f48f17c9957ec42ecbe56622db50
Tobias Haase <tobias.haase@dlr.de>
2022-10-18 06:25:50 +0000

    proper error handling when response is not a json

--- src/utils/error-handling.ts
+++ src/utils/error-handling.ts
@@ -2,12 +2,34 @@ import type { ResponseError } from "@dlr-shepard/shepard-client";
 import log from "loglevel";
 import { emitter, type ErrorType } from "./event-bus";
 
+function isErrorType(error: object): error is ErrorType {
+  return "status" in error && "exception" in error && "message" in error;
+}
+
+function isJsonString(str: string) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
 async function parseResponseError(error: ResponseError): Promise<ErrorType> {
   const result = await error.response.body?.getReader().read();
   if (result?.value) {
     const errorString = new TextDecoder().decode(result.value);
-    const errorObject = JSON.parse(errorString);
-    return errorObject as ErrorType;
+    let errorObject: ErrorType;
+    if (isJsonString(errorString) && isErrorType(JSON.parse(errorString))) {
+      errorObject = JSON.parse(errorString);
+    } else {
+      errorObject = {
+        status: error.response.status,
+        exception: error.response.statusText,
+        message: "",
+      };
+    }
+    return errorObject;
   }
   return {
     status: 400,
