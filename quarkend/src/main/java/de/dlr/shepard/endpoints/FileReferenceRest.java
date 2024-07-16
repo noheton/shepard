ceaa333c508bb7398df2228cad1493515c884048
Robin Laerm <robin.laerm@xitaso.com>
2024-07-16 17:29:07 +0200

    DESHN-27 replace io.swagger annotations with microprofile

--- quarkend/src/main/java/de/dlr/shepard/endpoints/FileReferenceRest.java
+++ quarkend/src/main/java/de/dlr/shepard/endpoints/FileReferenceRest.java
@@ -3,46 +3,46 @@ package de.dlr.shepard.endpoints;
 import de.dlr.shepard.mongoDB.ShepardFile;
 import de.dlr.shepard.neo4Core.io.FileReferenceIO;
 import de.dlr.shepard.util.Constants;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 public interface FileReferenceRest {
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Get all file references")
-  @ApiResponse(
+  @APIResponse(
     description = "ok",
     responseCode = "200",
-    content = @Content(array = @ArraySchema(schema = @Schema(implementation = FileReferenceIO.class)))
+    content = @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = FileReferenceIO.class))
   )
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "not found", responseCode = "404")
   Response getAllFileReferences(long collectionId, long dataObjectId);
 
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Get file reference")
-  @ApiResponse(
+  @APIResponse(
     description = "ok",
     responseCode = "200",
     content = @Content(schema = @Schema(implementation = FileReferenceIO.class))
   )
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "not found", responseCode = "404")
   Response getFileReference(long collectionId, long dataObjectId, long referenceId);
 
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Create a new file reference")
-  @ApiResponse(
+  @APIResponse(
     description = "created",
     responseCode = "201",
     content = @Content(schema = @Schema(implementation = FileReferenceIO.class))
   )
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "not found", responseCode = "404")
   Response createFileReference(
     long collectionId,
     long dataObjectId,
@@ -54,30 +54,30 @@ public interface FileReferenceRest {
 
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Delete file reference")
-  @ApiResponse(description = "deleted", responseCode = "204")
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "deleted", responseCode = "204")
+  @APIResponse(description = "not found", responseCode = "404")
   Response deleteFileReference(long collectionId, long dataObjectId, long fileReferenceId);
 
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Get file payload")
-  @ApiResponse(
+  @APIResponse(
     description = "ok",
     responseCode = "200",
     content = @Content(
       mediaType = MediaType.APPLICATION_OCTET_STREAM,
-      schema = @Schema(type = "string", format = "binary")
+      schema = @Schema(type = SchemaType.STRING, format = "binary")
     )
   )
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "not found", responseCode = "404")
   Response getFilePayload(long collectionId, long dataObjectId, long fileReferenceId, String oid);
 
   @Tag(name = Constants.FILE_REFERENCE)
   @Operation(description = "Get associated files")
-  @ApiResponse(
+  @APIResponse(
     description = "ok",
     responseCode = "200",
-    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ShepardFile.class)))
+    content = @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = ShepardFile.class))
   )
-  @ApiResponse(description = "not found", responseCode = "404")
+  @APIResponse(description = "not found", responseCode = "404")
   Response getFiles(long collectionId, long dataObjectId, long fileId);
 }
