4ad9ade65ad924c299df946a5860f1e2544fb4e1
Paul Reinhardt <paul.reinhardt@xitaso.com>
2024-08-15 11:39:28 +0200

    #143 Create minimal typescript script to compare clients exports

---
+++ clients/tests/typescript/src/compareExports.ts
@@ -0,0 +1,20 @@
+import * as client from "@dlr-shepard/shepard-client";
+import * as newClient from "../new-shepard-client/src/index";
+
+export const compareExports = () => {
+  const oldExports = Object.keys(client);
+  const newExports = Object.keys(newClient);
+
+  console.log("The following exports are not contained in the new client", "\n");
+  oldExports
+    .filter((v) => newExports.indexOf(v) < 0)
+    .forEach((k) => console.log(k, "(of type", typeof (client as any)[k] + ")"));
+
+  console.log("\n");
+  console.log("The following exports are added in the new client", "\n");
+  newExports
+    .filter((v) => oldExports.indexOf(v) < 0)
+    .forEach((k) => console.log(k, "(of type", typeof (newClient as any)[k] + ")"));
+};
+
+compareExports();
