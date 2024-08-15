c98ec4926e4b109123a6fa224ae96987b55b058f
Paul Reinhardt <paul.reinhardt@xitaso.com>
2024-08-15 14:05:23 +0200

    #143 Add error to the script

--- clients/tests/typescript/src/compareExports.ts
+++ clients/tests/typescript/src/compareExports.ts
@@ -5,16 +5,17 @@ export const compareExports = () => {
   const oldExports = Object.keys(client);
   const newExports = Object.keys(newClient);
 
-  console.log("The following exports are not contained in the new client", "\n");
-  oldExports
-    .filter((v) => newExports.indexOf(v) < 0)
-    .forEach((k) => console.log(k, "(of type", typeof (client as any)[k] + ")"));
+  console.log("The following exports are missing in the new client");
+  const missingExports = oldExports.filter((v) => newExports.indexOf(v) < 0);
+  missingExports.forEach((k) => console.log(" - ", k, "(of type", typeof (client as any)[k] + ")"));
 
-  console.log("\n");
-  console.log("The following exports are added in the new client", "\n");
-  newExports
-    .filter((v) => oldExports.indexOf(v) < 0)
-    .forEach((k) => console.log(k, "(of type", typeof (newClient as any)[k] + ")"));
+  console.log("The following exports are added in the new client");
+  const addedExports = newExports.filter((v) => oldExports.indexOf(v) < 0);
+  addedExports.forEach((k) => console.log(" - ", k, "(of type", typeof (newClient as any)[k] + ")"));
+
+  if (missingExports.length > 0) {
+    throw new Error("Some exports are missing in the new version!");
+  }
 };
 
 compareExports();
