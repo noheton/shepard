68f3926587a909224cd530704b32f8fe744d9b59
Mohamed Weslati <mohamed.weslati@xitaso.com>
2024-08-22 07:14:54 +0000

    #154 Switch to SEMVER

--- scripts/shepard_scripts/scripts/releases.py
+++ scripts/shepard_scripts/scripts/releases.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import re
+from collections import defaultdict
 
 import click
 import jinja2
@@ -12,6 +13,7 @@ DEPENDENCY_BUMP_LABEL = "dependencies"
 TEMPLATE_FILE = "templates/release_notes.md"
 MAIN_BRANCH = "main"
 DEV_BRANCH = "develop"
+SEMVER_PATTERN = r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)$"
 
 
 def _get_changes(
@@ -67,17 +69,6 @@ def _get_release_notes(
     return result
 
 
-def _get_latest_release_date(project: Project) -> str:
-    releases: list[ProjectRelease] = project.releases.list(per_page=1, page=0)  # type: ignore
-    return releases[0].released_at if len(releases) > 0 else "2001-01-01T00:00:00.000Z"
-
-
-def get_release_version() -> str:
-    # TODO: replace with semver, ask user
-    now = datetime.now()
-    return now.strftime("%Y.%m.%d")
-
-
 def get_project(gitlab_instance: str, token: str, project_id: int) -> Project:
     instance = Gitlab(gitlab_instance, token)
     try:
@@ -86,45 +77,100 @@ def get_project(gitlab_instance: str, token: str, project_id: int) -> Project:
         raise click.Abort(f"Project {ex} could not be found") from ex
 
 
-def get_release_notes(project: Project, version: str) -> tuple[str, str]:
-    latest_release = _get_latest_release_date(project)
-    breaking, _dependencies, others = _get_changes(project, latest_release)
+def _get_latest_release(project: Project) -> tuple[str, str]:
+    """Get latests release date and tag.
+    ---
+    Returns:
+    Tuple[str, str] : date, tag
+    """
+    releases: list[ProjectRelease] = project.releases.list(per_page=1, page=0)  # type: ignore
+    if not releases:
+        return ("2001-01-01T00:00:00.000Z", "0.0.0")
+    else:
+        return (releases[0].released_at, releases[0].tag_name)
 
+
+def _create_next_tag(latest_release_tag, has_breaking_changes: bool) -> str:
+    match = re.match(SEMVER_PATTERN, latest_release_tag)
+    version_dict = defaultdict(int)
+    if match:
+        version_dict["major"] = int(match.group("major"))
+        version_dict["minor"] = int(match.group("minor"))
+        version_dict["patch"] = int(match.group("patch"))
+    else:
+        click.echo(
+            "The last version is not SEMVER, switching to SEMVER starting at 1.0.0"
+        )
+        return "1.0.0"
+
+    release_type = "patch"
+    if has_breaking_changes:
+        click.echo(
+            "The release contains breaking changes!"
+            + " Automatically increasing 'major' version."
+        )
+        release_type = "major"
+    else:
+        click.echo(f"The current version tag is {latest_release_tag}.")
+        release_type = click.prompt(
+            "What is the next release type?",
+            type=click.Choice(["patch", "minor", "major"]),
+            default="patch",
+        )
+
+    # increase release number according to release type
+    version_dict[release_type] += 1
+    return f"{version_dict['major']}.{version_dict['minor']}.{version_dict['patch']}"
+
+
+def _create_release_notes(
+    project: Project,
+    breaking_changes: list[MergeRequest],
+    other_changes: list[MergeRequest],
+) -> str:
     click.echo({project.name_with_namespace})
     click.echo("Merge Requests:")
-    click.echo("\n".join([mr.title for mr in breaking + others]))
+    click.echo("\n".join([mr.title for mr in breaking_changes + other_changes]))
     click.echo()
 
-    release_name = click.prompt("Release name")
-    title = f"{version} {release_name}"
     release_notes = (
-        click.edit(_get_release_notes(breaking, others), require_save=False) or ""
+        click.edit(
+            _get_release_notes(breaking_changes, other_changes), require_save=False
+        )
+        or ""
     )
 
+    return release_notes
+
+
+def create_release_details(project: Project) -> tuple[str, str]:
+    latest_release_date, latest_release_tag = _get_latest_release(project)
+
+    breaking_changes, _dependencies, other_changes = _get_changes(
+        project, latest_release_date
+    )
+    release_notes = _create_release_notes(project, breaking_changes, other_changes)
+    next_tag = _create_next_tag(
+        latest_release_tag, has_breaking_changes=bool(breaking_changes)
+    )
+
+    return (next_tag, release_notes)
+
+
+def prompt_title(tag: str) -> str:
+    release_name = click.prompt("Release name")
+    title = f"{tag} {release_name}"
+    return title
+
+
+def prompt_confirm(title: str, tag: str, notes: str):
     click.echo(f"Title: {title}")
-    click.echo(f"Version: {version}")
+    click.echo(f"Tag: {tag}")
     click.echo("Release notes:")
-    click.echo(release_notes)
+    click.echo(notes)
     click.echo()
     if not click.confirm("OK?", abort=True):
         raise click.Abort("Not confirmed")
-
-    return (title, release_notes)
-
-
-def create_release_mr(project: Project, title: str):
-    mr = project.mergerequests.create(
-        {
-            "source_branch": DEV_BRANCH,
-            "target_branch": MAIN_BRANCH,
-            "title": title,
-            "description": f"This is the Merge Request for {title}",
-            "remove_source_branch": False,
-            "squash": False,
-        }
-    )
-    mr.merge()
-    click.echo("Merge Request created and merged")
 
 
 def create_release(project: Project, title: str, tag: str, notes: str):
