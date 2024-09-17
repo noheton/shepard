525d0950be0f26b168a62f4c811cfc4a8aff2550
Lukas Werner <lukas.werner@xitaso.com>
2024-09-17 08:56:35 +0200

    #223 Create new header bar for nuxtend

--- nuxtend/app.vue
+++ nuxtend/app.vue
@@ -1,16 +1,7 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <!-- Toolbar -->
-      <v-toolbar color="primary" dark>
-        <v-toolbar-title>Vuetify App Example</v-toolbar-title>
-        <v-btn icon>
-          <v-icon>mdi-bell</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </v-toolbar>
+      <HeaderBar />
       <NuxtPage />
     </v-app>
   </NuxtLayout>
---
+++ nuxtend/components/HeaderBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-toolbar>
+    <v-btn to="/">
+      <v-img
+        src="../assets/Shepard_Logo_1024x191.png"
+        height="25"
+        width="125"
+      />
+    </v-btn>
+    <v-btn to="/collections">Collections</v-btn>
+  </v-toolbar>
+</template>
