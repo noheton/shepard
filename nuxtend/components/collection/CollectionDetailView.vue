d669586139701ab9a1565fa2dcea3dbdacf8b9e8
Mohamed Weslati <mohamed.weslati@xitaso.com>
2024-09-26 21:18:04 +0000

    #226 Initial detail view fetch

--- nuxtend/components/collection/CollectionDetailView.vue
+++ nuxtend/components/collection/CollectionDetailView.vue
@@ -1,18 +1,61 @@
 <script setup lang="ts">
+import { DataObjectApi, type DataObject } from "@dlr-shepard/backend-client";
+
 interface CollectionDetailViewProps {
   collectionId: number;
   dataObjectId: number | undefined;
 }
-
 const props = defineProps<CollectionDetailViewProps>();
+
+const dataObject = ref<DataObject | undefined>(undefined);
+
+async function fetchDataObjectDetails() {
+  if (props.dataObjectId)
+    createApiInstance(DataObjectApi)
+      .getDataObject({
+        collectionId: props.collectionId,
+        dataObjectId: props.dataObjectId,
+      })
+      .then(response => {
+        dataObject.value = response;
+      })
+      .catch(error => {
+        handleError(error, "getDataObject");
+      });
+}
+
+watchEffect(() => {
+  if (props.dataObjectId) {
+    fetchDataObjectDetails();
+  } else {
+    dataObject.value = undefined;
+  }
+});
 </script>
 
 <template>
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-        Details for DataObject {{ props.dataObjectId }} in collection
+        Details for DataObject in collection
         {{ props.collectionId }}
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container v-if="dataObject" fluid>
+    <v-row>
+      <v-col cols="4">Collection ID: {{ dataObject.collectionId }}</v-col>
+      <v-col cols="4">
+        Created at: {{ dataObject.createdAt?.toDateString() }}
+      </v-col>
+      <v-col v-if="dataObject.updatedAt" cols="4">
+        Updated at: {{ dataObject.updatedAt?.toDateString() }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">Description:</v-col>
+      <v-col cols="12">
+        <span>{{ dataObject.description }}</span>
       </v-col>
     </v-row>
   </v-container>
