ba96729bd0642acc755c97817103f427e11c4562
mweslati <mohamed.weslati@xitaso.com>
2024-10-01 15:29:22 +0200

    #249 Revert App.vue

--- nuxtend/app.vue
+++ nuxtend/app.vue
@@ -2,36 +2,4 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <v-snackbar
-    v-model="errorAlert"
-    location="right"
-    timeout="-1"
-    max-height="80px"
-  >
-    Error while {{ errorSituation }}: {{ errorException }}
-    <br />
-    <small>
-      <i>{{ errorMessage }}</i>
-    </small>
-    <template #actions>
-      <v-btn color="pink" variant="text" @click="errorAlert = false">
-        Close
-      </v-btn>
-    </template>
-  </v-snackbar>
 </template>
-
-<script setup>
-import { useEventBus } from "@vueuse/core";
-const errorSituation = ref("");
-const errorException = ref("");
-const errorMessage = ref("");
-const errorAlert = defineModel({ type: Boolean, default: false });
-const bus = useEventBus(errorKey);
-bus.on(e => {
-  errorSituation.value = e.situation;
-  errorException.value = e.error.exception;
-  errorMessage.value = e.error.message;
-  errorAlert.value = true;
-});
-</script>
