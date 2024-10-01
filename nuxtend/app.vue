610ef4748ea4fe008115aa4dd49493ddaafabeea
mweslati <mohamed.weslati@xitaso.com>
2024-10-01 10:46:06 +0200

    Use recommended defineModel

--- nuxtend/app.vue
+++ nuxtend/app.vue
@@ -26,7 +26,8 @@ import { useEventBus } from "@vueuse/core";
 const errorSituation = ref("");
 const errorException = ref("");
 const errorMessage = ref("");
-const errorAlert = ref(false);
+// const errorAlert = ref(false);
+const errorAlert = defineModel({ type: Boolean, default: false });
 const bus = useEventBus(errorKey);
 bus.on(e => {
   errorSituation.value = e.situation;
