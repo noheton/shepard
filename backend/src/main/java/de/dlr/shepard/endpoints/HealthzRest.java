bd16cac959723aa0c9aa19a8e768301a8f58fa58
Maximilian Heykeroth <maximilian.heykeroth@xitaso.com>
2024-10-09 14:53:12 +0200

    #253 Replace MongoDBConnector with injected client in Healthz

--- backend/src/main/java/de/dlr/shepard/endpoints/HealthzRest.java
+++ backend/src/main/java/de/dlr/shepard/endpoints/HealthzRest.java
@@ -1,7 +1,17 @@
 package de.dlr.shepard.endpoints;
 
+import org.bson.Document;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+
 import de.dlr.shepard.influxDB.InfluxDBConnector;
-import de.dlr.shepard.mongoDB.MongoDBConnector;
+import de.dlr.shepard.mongoDB.MongoDBDatabaseNameService;
 import de.dlr.shepard.neo4Core.io.HealthzIO;
 import de.dlr.shepard.neo4j.NeoConnector;
 import de.dlr.shepard.util.Constants;
@@ -16,11 +26,6 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -31,14 +36,25 @@ public class HealthzRest {
   private static IConnector neo4j = NeoConnector.getInstance();
   private InfluxDBConnector influxdb;
 
-  private MongoDBConnector mongodb;
-
-  HealthzRest() {}
+  @Inject
+  MongoClient mongoClient;
 
   @Inject
-  public HealthzRest(InfluxDBConnector influxdb, MongoDBConnector mongodb) {
+  MongoDBDatabaseNameService mongoDBNameService;
+
+  @Inject
+  public HealthzRest(InfluxDBConnector influxdb) {
     this.influxdb = influxdb;
-    this.mongodb = mongodb;
+  }
+
+  private boolean getMongoDBHealth() {
+    Document result;
+    try {
+      result = mongoClient.getDatabase(mongoDBNameService.getName()).runCommand(new Document("buildInfo", "1"));
+    } catch (MongoException ex) {
+      return false;
+    }
+    return result.containsKey("ok");
   }
 
   @GET
@@ -56,10 +72,12 @@ public class HealthzRest {
   )
   public Response getServerHealth() {
     var neo4jAlive = neo4j.alive();
-    var mongodbAlive = mongodb.alive();
+
+    boolean isMongoDBHealthy = getMongoDBHealth();
+
     var influxdbAlive = influxdb.alive();
-    var result = new HealthzIO(neo4jAlive, mongodbAlive, influxdbAlive);
-    if (neo4jAlive && mongodbAlive && influxdbAlive) return Response.ok(result).build();
+    var result = new HealthzIO(neo4jAlive, isMongoDBHealthy, influxdbAlive);
+    if (neo4jAlive && isMongoDBHealthy && influxdbAlive) return Response.ok(result).build();
     Log.errorf("UNHEALTHY: %s", result);
     return Response.status(Status.SERVICE_UNAVAILABLE).entity(result).build();
   }
