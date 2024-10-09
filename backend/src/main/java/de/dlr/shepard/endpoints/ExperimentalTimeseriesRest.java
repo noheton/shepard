019361bb5ae4ebb32d60e7ef94b004bef38d1c45
Robin Laerm <robin.laerm@xitaso.com>
2024-10-09 14:44:39 +0200

    258 takeover of REST methods

--- backend/src/main/java/de/dlr/shepard/endpoints/ExperimentalTimeseriesRest.java
+++ backend/src/main/java/de/dlr/shepard/endpoints/ExperimentalTimeseriesRest.java
@@ -1,10 +1,17 @@
 package de.dlr.shepard.endpoints;
 
+import de.dlr.shepard.exceptions.InvalidRequestException;
 import de.dlr.shepard.filters.Subscribable;
+import de.dlr.shepard.influxDB.FillOption;
+import de.dlr.shepard.influxDB.SingleValuedUnaryFunction;
 import de.dlr.shepard.influxDB.Timeseries;
 import de.dlr.shepard.influxDB.TimeseriesPayload;
+import de.dlr.shepard.neo4Core.io.PermissionsIO;
+import de.dlr.shepard.neo4Core.io.RolesIO;
 import de.dlr.shepard.neo4Core.io.TimeseriesContainerIO;
 import de.dlr.shepard.neo4Core.orderBy.ContainerAttributes;
+import de.dlr.shepard.neo4Core.services.PermissionsService;
+import de.dlr.shepard.security.PermissionsUtil;
 import de.dlr.shepard.services.ExperimentalTimeseriesContainerService;
 import de.dlr.shepard.services.TimeseriesContainerIOMapper;
 import de.dlr.shepard.util.Constants;
@@ -15,14 +22,23 @@ import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.SecurityContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
@@ -32,6 +48,8 @@ import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -40,6 +58,8 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 public class ExperimentalTimeseriesRest {
 
   private ExperimentalTimeseriesContainerService timeseriesContainerService;
+  private PermissionsService permissionsService;
+  private PermissionsUtil permissionsUtil;
 
   @Context
   private SecurityContext securityContext;
@@ -49,10 +69,14 @@ public class ExperimentalTimeseriesRest {
   @Inject
   public ExperimentalTimeseriesRest(
     ExperimentalTimeseriesContainerService timeseriesContainerService,
-    SecurityContext securityContext
+    SecurityContext securityContext,
+    PermissionsService permissionsService,
+    PermissionsUtil permissionsUtil
   ) {
     this.timeseriesContainerService = timeseriesContainerService;
     this.securityContext = securityContext;
+    this.permissionsService = permissionsService;
+    this.permissionsUtil = permissionsUtil;
   }
 
   @GET
@@ -141,6 +165,7 @@ public class ExperimentalTimeseriesRest {
       securityContext.getUserPrincipal().getName()
     );
 
+    if (result == false) throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
     return result;
   }
 
@@ -164,6 +189,240 @@ public class ExperimentalTimeseriesRest {
     ) @Valid TimeseriesPayload payload
   ) {
     var timeseries = timeseriesContainerService.createTimeseries(timeseriesId, payload);
+
+    if (timeseries == null) throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
     return timeseries;
+  }
+
+  @GET
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.AVAILABLE)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Get timeseries available")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = Timeseries.class))
+  )
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  public List<Timeseries> getTimeseriesAvailable(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId
+  ) {
+    var timeserieses = timeseriesContainerService.getTimeseriesAvailable(timeseriesContainerId);
+    return timeserieses;
+  }
+
+  @GET
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.PAYLOAD)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Get timeseries payload")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(schema = @Schema(implementation = TimeseriesPayload.class))
+  )
+  @APIResponse(description = "not found", responseCode = "404")
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  @Parameter(name = Constants.MEASUREMENT, required = true)
+  @Parameter(name = Constants.LOCATION, required = true)
+  @Parameter(name = Constants.DEVICE, required = true)
+  @Parameter(name = Constants.SYMBOLICNAME, required = true)
+  @Parameter(name = Constants.FIELD, required = true)
+  @Parameter(name = Constants.START, required = true)
+  @Parameter(name = Constants.END, required = true)
+  @Parameter(name = Constants.FUNCTION)
+  @Parameter(name = Constants.GROUP_BY)
+  @Parameter(name = Constants.FILLOPTION)
+  public TimeseriesPayload getTimeseries(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId,
+    @QueryParam(Constants.MEASUREMENT) String measurement,
+    @QueryParam(Constants.LOCATION) String location,
+    @QueryParam(Constants.DEVICE) String device,
+    @QueryParam(Constants.SYMBOLICNAME) String symbolicName,
+    @QueryParam(Constants.FIELD) String field,
+    @QueryParam(Constants.START) long start,
+    @QueryParam(Constants.END) long end,
+    @QueryParam(Constants.FUNCTION) SingleValuedUnaryFunction function,
+    @QueryParam(Constants.GROUP_BY) Long groupBy,
+    @QueryParam(Constants.FILLOPTION) FillOption fillOption // Todo: how to replace filloption?
+  ) {
+    if (measurement == null || location == null || device == null || symbolicName == null || field == null) {
+      throw new InvalidRequestException("Some query params are missing");
+    }
+
+    var timeseries = new Timeseries(measurement, device, location, symbolicName, field);
+    var payload = timeseriesContainerService.getTimeseriesPayload(
+      timeseriesContainerId,
+      timeseries,
+      start,
+      end,
+      function,
+      groupBy,
+      fillOption
+    );
+
+    if (payload == null) throw new NotFoundException();
+    return payload;
+  }
+
+  @GET
+  @Produces({ MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_JSON })
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.EXPORT)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Export timeseries payload")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(
+      mediaType = MediaType.APPLICATION_OCTET_STREAM,
+      schema = @Schema(type = SchemaType.STRING, format = "binary")
+    )
+  )
+  @APIResponse(description = "not found", responseCode = "404")
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  @Parameter(name = Constants.MEASUREMENT, required = true)
+  @Parameter(name = Constants.LOCATION, required = true)
+  @Parameter(name = Constants.DEVICE, required = true)
+  @Parameter(name = Constants.SYMBOLICNAME, required = true)
+  @Parameter(name = Constants.FIELD, required = true)
+  @Parameter(name = Constants.START, required = true)
+  @Parameter(name = Constants.END, required = true)
+  @Parameter(name = Constants.FUNCTION)
+  @Parameter(name = Constants.GROUP_BY)
+  @Parameter(name = Constants.FILLOPTION)
+  public Response exportTimeseries(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId,
+    @QueryParam(Constants.MEASUREMENT) String measurement,
+    @QueryParam(Constants.LOCATION) String location,
+    @QueryParam(Constants.DEVICE) String device,
+    @QueryParam(Constants.SYMBOLICNAME) String symbolicName,
+    @QueryParam(Constants.FIELD) String field,
+    @QueryParam(Constants.START) long start,
+    @QueryParam(Constants.END) long end,
+    @QueryParam(Constants.FUNCTION) SingleValuedUnaryFunction function,
+    @QueryParam(Constants.GROUP_BY) Long groupBy,
+    @QueryParam(Constants.FILLOPTION) FillOption fillOption
+  ) throws IOException {
+    if (measurement == null || location == null || device == null || symbolicName == null || field == null) {
+      throw new InvalidRequestException("Some query params are missing");
+    }
+
+    var timeseries = new Timeseries(measurement, device, location, symbolicName, field);
+    var inputStream = timeseriesContainerService.exportTimeseriesPayload(
+      timeseriesContainerId,
+      timeseries,
+      start,
+      end,
+      function,
+      groupBy,
+      fillOption
+    );
+
+    if (inputStream == null) throw new NotFoundException();
+
+    return Response.ok(inputStream, MediaType.APPLICATION_OCTET_STREAM)
+      .header("Content-Disposition", "attachment; filename=\"timeseries-export.csv\"")
+      .build();
+  }
+
+  @POST
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.IMPORT)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Import timeseries payload")
+  @APIResponse(description = "ok", responseCode = "200")
+  @APIResponse(description = "not found", responseCode = "404")
+  @Subscribable
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  public void importTimeseries(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId,
+    MultipartBodyFileUpload body
+  ) throws IOException {
+    String filePath = body.fileUpload != null ? body.fileUpload.uploadedFile().toString() : null;
+
+    if (filePath == null) {
+      throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+    }
+
+    File file = new File(filePath);
+    try (InputStream fileInputStream = new FileInputStream(file)) {
+      var result = timeseriesContainerService.importTimeseries(timeseriesContainerId, fileInputStream);
+
+      if (result == false) throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GET
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.PERMISSIONS)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Get permissions")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(schema = @Schema(implementation = PermissionsIO.class))
+  )
+  @APIResponse(description = "not found", responseCode = "404")
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  public PermissionsIO getTimeseriesPermissions(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId
+  ) {
+    var permissions = permissionsService.getPermissionsByNeo4jId(timeseriesContainerId);
+    if (permissions == null) throw new NotFoundException();
+    return new PermissionsIO(permissions);
+  }
+
+  @PUT
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.PERMISSIONS)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Edit permissions")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(schema = @Schema(implementation = PermissionsIO.class))
+  )
+  @APIResponse(description = "not found", responseCode = "404")
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  public PermissionsIO editTimeseriesPermissions(
+    @PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId,
+    @RequestBody(
+      required = true,
+      content = @Content(schema = @Schema(implementation = PermissionsIO.class))
+    ) @Valid PermissionsIO permissions
+  ) {
+    var updatedPermissions = permissionsService.updatePermissionsByNeo4jId(permissions, timeseriesContainerId);
+    if (updatedPermissions == null) throw new NotFoundException();
+    return new PermissionsIO(updatedPermissions);
+  }
+
+  @GET
+  @Path("/{" + Constants.TIMESERIES_CONTAINER_ID + "}/" + Constants.ROLES)
+  @Tag(name = Constants.TIMESERIES_CONTAINER)
+  @Operation(description = "Get roles")
+  @APIResponse(
+    description = "ok",
+    responseCode = "200",
+    content = @Content(schema = @Schema(implementation = RolesIO.class))
+  )
+  @APIResponse(description = "not found", responseCode = "404")
+  @Parameter(name = Constants.TIMESERIES_CONTAINER_ID)
+  public RolesIO getTimeseriesRoles(@PathParam(Constants.TIMESERIES_CONTAINER_ID) long timeseriesContainerId) {
+    var roles = permissionsUtil.getRolesByNeo4jId(timeseriesContainerId, securityContext.getUserPrincipal().getName());
+    if (roles == null) throw new NotFoundException();
+    return roles;
+  }
+
+  @Schema(type = SchemaType.STRING, format = "binary", description = "Timeseries as CSV")
+  public interface UploadItemSchema {}
+
+  public class UploadFormSchema {
+
+    @Schema(required = true)
+    public UploadItemSchema file;
+  }
+
+  @Schema(implementation = UploadFormSchema.class)
+  public static class MultipartBodyFileUpload {
+
+    @RestForm(Constants.FILE)
+    public FileUpload fileUpload;
   }
 }
--- backend/src/main/java/de/dlr/shepard/services/ExperimentalTimeseriesContainerService.java
+++ backend/src/main/java/de/dlr/shepard/services/ExperimentalTimeseriesContainerService.java
@@ -1,5 +1,7 @@
 package de.dlr.shepard.services;
 
+import de.dlr.shepard.influxDB.FillOption;
+import de.dlr.shepard.influxDB.SingleValuedUnaryFunction;
 import de.dlr.shepard.influxDB.Timeseries;
 import de.dlr.shepard.influxDB.TimeseriesPayload;
 import de.dlr.shepard.neo4Core.dao.TimeseriesContainerDAO;
@@ -9,6 +11,8 @@ import de.dlr.shepard.util.QueryParamHelper;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.apache.commons.lang3.NotImplementedException;
 
@@ -104,5 +108,105 @@ public class ExperimentalTimeseriesContainerService {
     //   return null;
     // }
     // return payload.getTimeseries();
+  }
+
+  /**
+   * Returns a list of timeseries objects that are in the given database.
+   *
+   * @param timeseriesContainerId the given timeseries container
+   * @return a list of timeseries objects
+   */
+  public List<Timeseries> getTimeseriesAvailable(long timeseriesContainerId) {
+    throw new NotImplementedException();
+    // var timeseriesContainer = timeseriesContainerDAO.findLightByNeo4jId(timeseriesContainerId);
+    // if (timeseriesContainer == null || timeseriesContainer.isDeleted()) {
+    //   Log.errorf("Timeseries Container with id %s is null or deleted", timeseriesContainerId);
+    //   return Collections.emptyList();
+    // }
+    // return timeseriesService.getTimeseriesAvailable(timeseriesContainer.getDatabase());
+  }
+
+  /**
+   * Loads timeseries payload from a timeseries container.
+   *
+   * @param timeseriesContainerId identifies the TimeseriesContainer
+   * @param timeseries            The timeseries to load
+   * @param start                 The beginning of the timeseries
+   * @param end                   The end of the timeseries
+   * @param function              The aggregate function
+   * @param groupBy               The time interval measurements get grouped by
+   * @param fillOption            The fill option for missing values
+   * @return TimeseriesPayload
+   */
+  public TimeseriesPayload getTimeseriesPayload(
+    long timeseriesContainerId,
+    Timeseries timeseries,
+    long start,
+    long end,
+    SingleValuedUnaryFunction function,
+    Long groupBy,
+    FillOption fillOption
+  ) {
+    throw new NotImplementedException();
+    // var timeseriesContainer = timeseriesContainerDAO.findLightByNeo4jId(timeseriesContainerId);
+    // if (timeseriesContainer == null || timeseriesContainer.isDeleted()) {
+    //   Log.errorf("Timeseries Container with id %s is null or deleted", timeseriesContainerId);
+    //   return null;
+    // }
+    // var result = timeseriesService.getTimeseriesPayload(
+    //   start,
+    //   end,
+    //   timeseriesContainer.getDatabase(),
+    //   timeseries,
+    //   function,
+    //   groupBy,
+    //   fillOption
+    // );
+    // return result;
+  }
+
+  public InputStream exportTimeseriesPayload(
+    long timeseriesContainerId,
+    Timeseries timeseries,
+    long start,
+    long end,
+    SingleValuedUnaryFunction function,
+    Long groupBy,
+    FillOption fillOption
+  ) throws IOException {
+    throw new NotImplementedException();
+    // var timeseriesContainer = timeseriesContainerDAO.findLightByNeo4jId(timeseriesContainerId);
+    // if (timeseriesContainer == null || timeseriesContainer.isDeleted()) {
+    //   Log.errorf("Timeseries Container with id %s is null or deleted", timeseriesContainerId);
+    //   return null;
+    // }
+    // var result = timeseriesService.exportTimeseriesPayload(
+    //   start,
+    //   end,
+    //   timeseriesContainer.getDatabase(),
+    //   List.of(timeseries),
+    //   function,
+    //   groupBy,
+    //   fillOption,
+    //   Collections.emptySet(),
+    //   Collections.emptySet(),
+    //   Collections.emptySet()
+    // );
+    // return result;
+  }
+
+  public boolean importTimeseries(long timeseriesContainerId, InputStream stream) throws IOException {
+    throw new NotImplementedException();
+    // var timeseriesContainer = timeseriesContainerDAO.findLightByNeo4jId(timeseriesContainerId);
+    // if (timeseriesContainer == null || timeseriesContainer.isDeleted()) {
+    //   Log.errorf("Timeseries Container with id %s is null or deleted", timeseriesContainerId);
+    //   return false;
+    // }
+    // var result = timeseriesService.importTimeseries(timeseriesContainer.getDatabase(), stream);
+    // if (!result.isBlank()) {
+    //   Log.errorf("Failed to import timeseries with error: %s", result);
+    //   return false;
+    // }
+    // return true;
   }
 }
