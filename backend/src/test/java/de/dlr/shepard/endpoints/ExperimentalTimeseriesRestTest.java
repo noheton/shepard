e82025282dd0e9e5b7cf9c0c907519fce21dd66d
Robin Laerm <robin.laerm@xitaso.com>
2024-10-10 09:48:27 +0200

    258 first test skeleton of rest endpoint

---
+++ backend/src/test/java/de/dlr/shepard/endpoints/ExperimentalTimeseriesRestTest.java
@@ -0,0 +1,16 @@
+package de.dlr.shepard.endpoints;
+
+import static io.restassured.RestAssured.given;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.Test;
+
+@QuarkusTest
+public class ExperimentalTimeseriesRestTest {
+
+  @Test
+  public void testGetAllTimeseriesesContainersEndpoint() {
+    // Todo: Solve authentication problem
+    given().when().get("/experimental-timeseriesContainers").then().statusCode(404);
+  }
+}
