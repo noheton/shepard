b4375d24104c6925dc87235e1a73312a307828a0
mweslati <mohamed.weslati@xitaso.com>
2024-10-11 17:49:06 +0200

    #258 Update database schema

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -7,36 +7,39 @@ DROP TABLE IF EXISTS timeseries_payload CASCADE;
 -- Create timeseries table
 CREATE TABLE timeseries
 (
-    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    container_id BIGINT NOT NULL,
+    id          serial PRIMARY KEY,
+    container_id INTEGER NOT NULL,
     measurement TEXT NOT NULL,
-    device      TEXT NULL,
-    location    TEXT NULL,
+    field       TEXT NOT NULL,
     symbolic_name TEXT NULL,
-    field       TEXT NULL
+    device      TEXT NULL,
+    location    TEXT NULL
 );
 
 -- Create timeseries_payload table
 CREATE TABLE timeseries_payload
 (
-    id            UUID DEFAULT gen_random_uuid(),
-    timeseries_id UUID REFERENCES timeseries (id),
+    -- id            UUID DEFAULT gen_random_uuid(),
+    timeseries_id INTEGER REFERENCES timeseries (id),
     time          TIMESTAMPTZ NOT NULL,
-    value         DOUBLE PRECISION
+    float_value         DOUBLE PRECISION NULL,
+    int_value         INTEGER NULL,
+    string_value         TEXT NULL,
+    boolean_value         boolean NULL
 );
 
 -- Create hypertable for timeseries_payload on time
 SELECT create_hypertable('timeseries_payload', 'time');
 
 -- Insert initial data into timeseries table
-INSERT INTO timeseries ( measurement, device, location, container_id, id)
-VALUES ('Measurement 1', 'Robot 1', 'Factory Hall 1', 11, 'befd90a8-293a-49ab-81cb-23be2a9d8207'),
-       ('Measurement 1', 'Robot 2', 'Factory Hall 1', 11, gen_random_uuid()),
-       ('Measurement 2', 'Robot 2', 'Factory Hall 1', 11, gen_random_uuid());
+INSERT INTO timeseries ( measurement, device, location, field, container_id )
+VALUES ('Measurement 1', 'Robot 1', 'Factory Hall 1', 'value', 11),
+       ('Measurement 1', 'Robot 2', 'Factory Hall 1', 'value', 11),
+       ('Measurement 2', 'Robot 2', 'Factory Hall 1', 'value', 11);
 
 -- Insert initial data into timeseries_payload table with random values
-INSERT INTO timeseries_payload (time, timeseries_id, value)
+INSERT INTO timeseries_payload (time, timeseries_id, float_value)
 SELECT generate_series(now() - INTERVAL '24 hours', now(), INTERVAL '5 minutes'),
-       'befd90a8-293a-49ab-81cb-23be2a9d8207',
+       1,
        random() * 100
 FROM generate_series(1, 100);
