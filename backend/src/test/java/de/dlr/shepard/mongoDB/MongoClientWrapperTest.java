a28380b744cea6018b4c2cb16220a81ea08b4d33
Maximilian Heykeroth <maximilian.heykeroth@xitaso.com>
2024-10-11 11:10:41 +0200

    test unit test

--- backend/src/test/java/de/dlr/shepard/mongoDB/MongoClientWrapperTest.java
+++ backend/src/test/java/de/dlr/shepard/mongoDB/MongoClientWrapperTest.java
@@ -1,27 +1,21 @@
 package de.dlr.shepard.mongoDB;
 
 import static org.junit.Assert.assertEquals;
-
-import io.quarkus.test.component.QuarkusComponentTest;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
-@QuarkusComponentTest
 public class MongoClientWrapperTest {
-
-  @Inject
-  MongoClientWrapper mongoClientWrapper;
 
   @Test
   public void determineDatabaseNameWithDefaultDatabaseNameTest() {
-    assertEquals(mongoClientWrapper.determineDatabaseName("mongodb://mongo:shepard@[::1]:27017"), "database");
+    String databaseName = MongoClientWrapper.determineDatabaseName("mongodb://mongo:shepard@[::1]:27017");
+    assertEquals(databaseName, "database");
   }
 
   @Test
   public void determineDatabaseNameWithCustomDatabaseNameTest() {
-    assertEquals(
-      mongoClientWrapper.determineDatabaseName("mongodb://mongo:shepard@[::1]:27017/myVeryCoolDatabaseName"),
-      "myVeryCoolDatabaseName"
+    String databaseName = MongoClientWrapper.determineDatabaseName(
+      "mongodb://mongo:shepard@[::1]:27017/myVeryCoolDatabaseName"
     );
+    assertEquals(databaseName, "myVeryCoolDatabaseName");
   }
 }
