2791097a7ba0a7bca5f3b7c07cd9247eae19d0e9
Lukas Werner <lukas.werner@xitaso.com>
2024-10-29 13:08:30 +0000

    #255 Shepard favicon & Title

--- nuxtend/nuxt.config.ts
+++ nuxtend/nuxt.config.ts
@@ -46,6 +46,12 @@ export default defineNuxtConfig({
     },
   },
 
+  app: {
+    head: {
+      title: "Shepard",
+    },
+  },
+
   // This reverts the new srcDir default from `app` back to your root directory
   srcDir: ".",
   // This specifies the directory prefix for `app/router.options.ts` and `app/spa-loading-template.html`
