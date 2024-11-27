ef42e0aca4b250e5c5d4dae77e55bc6df3b01271
xit-eyad-aljawabreh <eyad.al-jawabreh@xitaso.com>
2024-11-27 20:28:02 +0100

    #323 Added created by, at, updated by, at to the IO class

--- backend/src/main/java/de/dlr/shepard/labJournal/io/LabJournalIO.java
+++ backend/src/main/java/de/dlr/shepard/labJournal/io/LabJournalIO.java
@@ -1,21 +1,15 @@
 package de.dlr.shepard.labJournal.io;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import de.dlr.shepard.labJournal.entities.LabJournal;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
+import java.util.Date;
 import lombok.Data;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Data
-@AllArgsConstructor
 @Schema(name = "LabJournal")
 public class LabJournalIO {
-
-  public LabJournalIO(LabJournal labJournal) {
-    this.dataObjectId = labJournal.getDataObject().getShepardId();
-    this.journalContent = labJournal.getDescription();
-    this.labJournalId = labJournal.getShepardId();
-  }
 
   @NotNull
   private long dataObjectId;
@@ -23,6 +17,38 @@ public class LabJournalIO {
   @NotNull
   private String journalContent;
 
-  @NotNull
-  private long labJournalId;
+  @Schema(readOnly = true, required = true)
+  private Long id;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  @Schema(readOnly = true, required = true, format = "date-time", example = "2024-08-15T11:18:44.632+00:00")
+  private Date createdAt;
+
+  @Schema(readOnly = true, required = true)
+  private String createdBy;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  @Schema(
+    readOnly = true,
+    nullable = true,
+    required = true,
+    format = "date-time",
+    example = "2024-08-15T11:18:44.632+00:00"
+  )
+  private Date updatedAt;
+
+  @Schema(readOnly = true, nullable = true, required = true)
+  private String updatedBy;
+
+  public LabJournalIO() {}
+
+  public LabJournalIO(LabJournal labJournal) {
+    this.dataObjectId = labJournal.getDataObject().getShepardId();
+    this.journalContent = labJournal.getDescription();
+    this.id = labJournal.getShepardId();
+    this.createdAt = labJournal.getCreatedAt();
+    this.createdBy = labJournal.getCreatedBy() != null ? labJournal.getCreatedBy().getUsername() : null;
+    this.updatedAt = labJournal.getUpdatedAt();
+    this.updatedBy = labJournal.getUpdatedBy() != null ? labJournal.getUpdatedBy().getUsername() : null;
+  }
 }
