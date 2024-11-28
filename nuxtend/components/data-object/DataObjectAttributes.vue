e8dc12cdc267bf902e0f3e8be23a814c8e20ab9c
Mohamed Weslati <mohamed.weslati@xitaso.com>
2024-11-28 10:34:47 +0000

    #336 Render & Style Attributes

--- nuxtend/components/data-object/DataObjectAttributes.vue
+++ nuxtend/components/data-object/DataObjectAttributes.vue
@@ -1,19 +1,47 @@
 <script setup lang="ts">
 import type { DataObject } from "@dlr-shepard/backend-client";
+const NUMBER_OF_ITEMS_TO_DISPLAY_WHEN_NOT_SHOWING_ALL = 7;
 
 interface DataObjectAttributesProps {
   dataObject: DataObject;
 }
-defineProps<DataObjectAttributesProps>();
+const props = defineProps<DataObjectAttributesProps>();
+
+const attributeKeysToDisplay = computed(() => {
+  const dataObjectAttributes = Object.keys(props.dataObject.attributes ?? {});
+  if (
+    viewAllAttributes.value ||
+    dataObjectAttributes.length <=
+      NUMBER_OF_ITEMS_TO_DISPLAY_WHEN_NOT_SHOWING_ALL
+  )
+    return dataObjectAttributes;
+  return dataObjectAttributes.slice(
+    0,
+    NUMBER_OF_ITEMS_TO_DISPLAY_WHEN_NOT_SHOWING_ALL,
+  );
+});
+
+const showViewAllBtn = computed(
+  () =>
+    Object.keys(props.dataObject.attributes ?? {}).length >
+    NUMBER_OF_ITEMS_TO_DISPLAY_WHEN_NOT_SHOWING_ALL,
+);
+
+const viewAllAttributes = ref(false);
 </script>
 
 <template>
   <v-container fluid>
-    <v-row
-      v-for="(attributeKey, index) in Object.keys(dataObject.attributes ?? {})"
-      :key="index"
-    >
-      {{ attributeKey }}: {{ dataObject.attributes?.[attributeKey] }}
+    <v-row v-for="(attributeKey, index) in attributeKeysToDisplay" :key="index">
+      <v-col cols="2" class="text-body-2 black-600 px-0 py-1">
+        {{ attributeKey }}:
+      </v-col>
+      <v-col class="text-body-2 black-600 px-0 py-1">
+        {{ dataObject.attributes?.[attributeKey] }}
+      </v-col>
+    </v-row>
+    <v-row v-if="showViewAllBtn">
+      <DesignComponentsViewAllButton v-model:view-all="viewAllAttributes" />
     </v-row>
   </v-container>
 </template>
---
+++ nuxtend/components/design-components/ViewAllButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const viewAll = defineModel<boolean>("viewAll", {
+  required: true,
+});
+</script>
+
+<template>
+  <v-btn
+    class="ml-15"
+    :prepend-icon="viewAll ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+    variant="plain"
+    base-color="blue-500"
+    :ripple="false"
+    @click="viewAll = !viewAll"
+  >
+    View all
+  </v-btn>
+</template>
+
+<style scoped>
+.v-btn--variant-plain {
+  opacity: unset;
+}
+</style>
