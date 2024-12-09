09de5b2dfc21795540304cc34e1061caac53dbc7
mweslati <mohamed.weslati@xitaso.com>
2024-12-09 15:02:17 +0100

    #208 Adapt invalid html error response

--- backend/src/main/java/de/dlr/shepard/exceptions/InvalidHtmlResponse.java
+++ backend/src/main/java/de/dlr/shepard/exceptions/InvalidHtmlResponse.java
@@ -1,5 +1,6 @@
 package de.dlr.shepard.exceptions;
 
+import jakarta.ws.rs.core.Response.Status;
 import lombok.Getter;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -11,16 +12,22 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 public class InvalidHtmlResponse {
 
   @Schema(readOnly = true)
-  private String message =
+  private final String message =
     "You provided invalid or unsecure Html to the Api. " +
     "Please make sure that your provided Html does not contain any invalid tags or attributes. " +
     "See the 'sanitizedHtml' field to find out which tags and attributes were removed during sanitization.";
 
   @Schema(readOnly = true)
-  private String suppliedHtml;
+  private final String exception = "BadRequestException";
 
   @Schema(readOnly = true)
-  private String sanitizedHtml;
+  private final Status status = Status.BAD_REQUEST;
+
+  @Schema(readOnly = true)
+  private final String suppliedHtml;
+
+  @Schema(readOnly = true)
+  private final String sanitizedHtml;
 
   public InvalidHtmlResponse(String suppliedHtml, String sanitizedHtml) {
     this.suppliedHtml = suppliedHtml;
