537b7ac9f752e868dcc1d684cd286f09559dff40
Lukas Werner <lukas.werner@xitaso.com>
2025-01-15 07:03:54 +0000

    #401 added horizontal scrollbar

--- nuxtend/layouts/collection.vue
+++ nuxtend/layouts/collection.vue
@@ -9,12 +9,12 @@ const routeParams = getCollectionRouterParamsFromRoute(route.params)!;
 
 <template>
   <DefaultLayout>
-    <v-container fluid class="pa-0 fill-height align-start">
+    <v-container fluid class="pa-0 fill-height align-start overflow-x-auto">
       <v-row no-gutters class="fill-height">
         <v-col cols="3">
           <CollectionSidebar :collection-route-params="routeParams" />
         </v-col>
-        <v-col cols="9" class="pa-8 overflow-x-auto"><slot /></v-col>
+        <v-col cols="9" class="pa-8"><slot /></v-col>
       </v-row>
     </v-container>
   </DefaultLayout>
