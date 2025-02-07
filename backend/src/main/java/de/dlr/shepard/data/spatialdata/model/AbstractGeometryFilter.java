5cc0a32b136a93457a4b3dee1c17d1d4584e3f53
mweslati <mohamed.weslati@xitaso.com>
2025-02-07 12:36:56 +0100

    Really fix the java client

--- backend/src/main/java/de/dlr/shepard/data/spatialdata/model/AbstractGeometryFilter.java
+++ backend/src/main/java/de/dlr/shepard/data/spatialdata/model/AbstractGeometryFilter.java
@@ -3,7 +3,7 @@ package de.dlr.shepard.data.spatialdata.model;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
-import org.eclipse.microprofile.openapi.annotations.media.DiscriminatorMapping;
+import lombok.NoArgsConstructor;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
@@ -15,20 +15,12 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
     @JsonSubTypes.Type(value = AxisAlignedBoundingBox.class, name = "AXIS_ALIGNED_BOUNDING_BOX"),
   }
 )
-@Schema(
-  description = "abstract geometry filter",
-  discriminatorProperty = "type",
-  discriminatorMapping = {
-    @DiscriminatorMapping(schema = KNearestNeighbor.class, value = "K_NEAREST_NEIGHBOR"),
-    @DiscriminatorMapping(schema = BoundingSphere.class, value = "BOUNDING_SPHERE"),
-    @DiscriminatorMapping(schema = OrientedBoundingBox.class, value = "ORIENTED_BOUNDING_BOX"),
-    @DiscriminatorMapping(schema = AxisAlignedBoundingBox.class, value = "AXIS_ALIGNED_BOUNDING_BOX"),
-  }
-)
+@Schema(description = "abstract geometry filter")
 @Data
+@NoArgsConstructor
 public abstract class AbstractGeometryFilter {
 
-  private GeometryFilterType type;
+  protected GeometryFilterType type;
 
   public AbstractGeometryFilter(GeometryFilterType type) {
     this.type = type;
