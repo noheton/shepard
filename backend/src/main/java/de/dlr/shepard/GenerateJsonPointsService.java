28fd2cad4254e6abb0d3c0049ee47868be526015
xit-eyad-aljawabreh <eyad.al-jawabreh@xitaso.com>
2025-02-07 09:59:08 +0100

    #416 Added generated details

--- backend/src/main/java/de/dlr/shepard/GenerateJsonPointsService.java
+++ backend/src/main/java/de/dlr/shepard/GenerateJsonPointsService.java
@@ -5,61 +5,110 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.logging.Log;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class GenerateJsonPointsService {
 
-  public static void generateJsonFile(int n, String fileName) {
+  static final double precision = 0.01;
+
+  public enum Type {
+    TAPE_LAYING,
+    ULTRASONIC,
+  }
+
+  public static void generateJsonFile(int n, String fileName, Type type) {
     ObjectMapper mapper = new ObjectMapper();
-    ArrayNode pointsArray = mapper.createArrayNode();
     Random random = new Random();
 
-    for (int i = 0; i < n; i++) {
-      ObjectNode pointNode = mapper.createObjectNode();
+    int track = 0; //increases every 1000 points
+    int layer = 0; //increases every 100 tracks
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    double excitationFrequency = 0; //  1-10 khz <increasing every 1_000_000> points
 
-      // Location
-      ObjectNode locationNode = mapper.createObjectNode();
-      pointNode.put("x", getRandomDouble(random));
-      pointNode.put("y", getRandomDouble(random));
-      pointNode.put("z", getRandomDouble(random));
-      //pointNode.set("location", locationNode);
+    try (FileWriter writer = new FileWriter(new File(fileName))) {
+      writer.write("[");
 
-      // Metadata
-      ObjectNode metadataNode = mapper.createObjectNode();
-      metadataNode.put("track", random.nextInt(200));
-      metadataNode.put("layer", random.nextInt(10));
+      for (int i = 0; i < n; i++) {
+        ObjectNode pointNode = mapper.createObjectNode();
 
-      // Base
-      ObjectNode baseNode = mapper.createObjectNode();
-      baseNode.put("X", getRandomDouble(random));
-      baseNode.put("Y", getRandomDouble(random));
-      baseNode.put("Z", getRandomDouble(random));
-      baseNode.put("A", getRandomDouble(random));
-      baseNode.put("B", getRandomDouble(random));
-      baseNode.put("C", getRandomDouble(random));
-      metadataNode.set("base", baseNode);
+        // Location
+        pointNode.put("x", x);
+        pointNode.put("y", y);
+        pointNode.put("z", z);
 
-      pointNode.set("metadata", metadataNode);
+        x += precision;
 
-      // Measurements
-      ObjectNode measurementsNode = mapper.createObjectNode();
-      ArrayNode dataArray = mapper.createArrayNode();
-      IntStream.range(1, 500).forEach(s -> {
-        dataArray.add(random.nextInt(s));
-      });
+        // Metadata
+        ObjectNode metadataNode = mapper.createObjectNode();
+        ObjectNode measurementsNode = mapper.createObjectNode();
+        switch (type) {
+          case TAPE_LAYING:
+            if (i % 1000 == 0) {
+              y += precision;
+              track++;
+            }
+            if (track % 100 == 0) {
+              layer++;
 
-      measurementsNode.set("data", dataArray);
-      pointNode.set("measurements", measurementsNode);
+              z += precision;
+            }
+            metadataNode.put("track", track);
+            metadataNode.put("layer", layer);
 
-      pointsArray.add(pointNode);
-    }
+            // Base
+            ObjectNode baseNode = mapper.createObjectNode();
+            baseNode.put("X", getRandomDouble(random));
+            baseNode.put("Y", getRandomDouble(random));
+            baseNode.put("Z", getRandomDouble(random));
+            baseNode.put("A", getRandomDouble(random));
+            baseNode.put("B", getRandomDouble(random));
+            baseNode.put("C", getRandomDouble(random));
+            metadataNode.set("base", baseNode);
 
-    try {
-      mapper.writerWithDefaultPrettyPrinter().writeValue(new File(fileName), pointsArray);
-      Log.info("JSON file generated: " + fileName);
+            // Measurements
+            ArrayNode tapeLayingDataArray = mapper.createArrayNode();
+            IntStream.range(1, 500).forEach(s -> {
+              tapeLayingDataArray.add(random.nextInt(100) - 50);
+            });
+
+            measurementsNode.set("data", tapeLayingDataArray);
+            break;
+          case ULTRASONIC:
+            metadataNode.put("excitation_frequency", excitationFrequency);
+            if (i % 1000_000 == 0) excitationFrequency++;
+            ArrayNode ultrasonicDataArray = mapper.createArrayNode();
+            IntStream.range(1, 500).forEach(s -> {
+              ultrasonicDataArray.add(random.nextInt(100) - 50);
+            });
+            measurementsNode.put("dampening", random.nextDouble());
+            measurementsNode.set("data", ultrasonicDataArray);
+
+            break;
+        }
+
+        pointNode.set("metadata", metadataNode);
+        pointNode.set("measurements", measurementsNode);
+
+        String jsonPoint = "";
+
+        jsonPoint = mapper.writeValueAsString(pointNode);
+
+        writer.write(jsonPoint);
+        if (i < n - 1) {
+          writer.write(",");
+        }
+      }
+
+      writer.write("]"); // Close JSON array
+      Log.info("file generated: " + fileName);
     } catch (IOException e) {
+      Log.error("Error while exporting file");
+      Log.error(e);
       e.printStackTrace();
     }
   }
--- backend/src/test/java/de/dlr/shepard/GenerateJsonPointsServiceTest.java
+++ backend/src/test/java/de/dlr/shepard/GenerateJsonPointsServiceTest.java
@@ -6,6 +6,8 @@ public class GenerateJsonPointsServiceTest {
 
   @Test
   public void generateJsonFileTest() {
-    GenerateJsonPointsService.generateJsonFile(10, "sample_points.json");
+    // UNCOMMENT to generate the output
+    //GenerateJsonPointsService.generateJsonFile(10, "sample_points_tape_laying.json", Type.TAPE_LAYING);
+    //GenerateJsonPointsService.generateJsonFile(20, "sample_points_ultrasonic.json", Type.ULTRASONIC);
   }
 }
