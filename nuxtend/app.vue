165f2b4235f1d38f9e082189052d367cd37c583c
Lukas Werner <lukas.werner@xitaso.com>
2025-02-17 14:42:29 +0000

    #251 Fix persistent dark mode

--- nuxtend/app.vue
+++ nuxtend/app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+onMounted(() => {
+  theme.global.name.value = localStorage.getItem("colorScheme") ?? "light";
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
