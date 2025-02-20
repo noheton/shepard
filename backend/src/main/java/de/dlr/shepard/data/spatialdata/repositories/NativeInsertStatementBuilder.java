d23edb622bb425a04745d86a816113ea8b72f190
Robin Laerm <robin.laerm@xitaso.com>
2025-02-20 12:49:38 +0000

    Add batch insert for spatial data

---
+++ backend/src/main/java/de/dlr/shepard/data/spatialdata/repositories/NativeInsertStatementBuilder.java
@@ -0,0 +1,31 @@
+package de.dlr.shepard.data.spatialdata.repositories;
+
+public class NativeInsertStatementBuilder {
+
+  private StringBuilder sb = new StringBuilder();
+  private boolean valuesAlreadyAdded = false;
+
+  public NativeInsertStatementBuilder insert(String insertStatement) {
+    sb.append(insertStatement);
+    return this;
+  }
+
+  public NativeInsertStatementBuilder addValues(String values) {
+    addValuesIfNecessary();
+    sb.append(String.format("(%s),", values));
+    return this;
+  }
+
+  public String build() {
+    sb.setLength(sb.length() - 1); // remove last ,
+    sb.append(";");
+    return sb.toString();
+  }
+
+  private void addValuesIfNecessary() {
+    if (valuesAlreadyAdded == false) {
+      sb.append(" values ");
+      valuesAlreadyAdded = true;
+    }
+  }
+}
---
+++ backend/src/test/java/de/dlr/shepard/data/spatialdata/repositories/NativeInsertStatementBuilderTest.java
@@ -0,0 +1,24 @@
+package de.dlr.shepard.data.spatialdata.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class NativeInsertStatementBuilderTest {
+
+  @Test
+  public void build_addOneLineOfValues_success() {
+    NativeInsertStatementBuilder builder = new NativeInsertStatementBuilder();
+    builder.insert("INSERT INTO table_name (column1, column2)").addValues("value1, value2");
+    String result = builder.build();
+    assertEquals("INSERT INTO table_name (column1, column2) values (value1, value2);", result);
+  }
+
+  @Test
+  public void build_multipleValuesAdded_success() {
+    NativeInsertStatementBuilder builder = new NativeInsertStatementBuilder();
+    builder.insert("INSERT INTO table_name (column1, column2)").addValues("value1, value2").addValues("value3, value4");
+    String result = builder.build();
+    assertEquals("INSERT INTO table_name (column1, column2) values (value1, value2),(value3, value4);", result);
+  }
+}
