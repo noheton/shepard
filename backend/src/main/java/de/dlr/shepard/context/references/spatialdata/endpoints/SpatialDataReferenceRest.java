1d84de57ec99825b602a55945582c140f34af008
xit-eyad-aljawabreh <eyad.al-jawabreh@xitaso.com>
2025-03-11 10:37:58 +0100

    #518 Added link in spatial reference post description

--- backend/src/main/java/de/dlr/shepard/context/references/spatialdata/endpoints/SpatialDataReferenceRest.java
+++ backend/src/main/java/de/dlr/shepard/context/references/spatialdata/endpoints/SpatialDataReferenceRest.java
@@ -125,7 +125,8 @@ public class SpatialDataReferenceRest {
     @PathParam(Constants.DATA_OBJECT_ID) long dataObjectId,
     @RequestBody(
       required = true,
-      description = "For more examples take a look at the endpoint '/spatialDataContainers/{id}/payload' .",
+      description = "For more examples take a look at [Get spatial data by container id](#/spatialDataContainer/getSpatialDataPoints).",
+
       content = @Content(schema = @Schema(implementation = SpatialDataReferenceIO.class))
     ) @Valid SpatialDataReferenceIO spatialDataReference
   ) {
