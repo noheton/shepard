d33c26e19b380712af26cb90daccae7e1782dbe5
Lukas Werner <lukas.werner@xitaso.com>
2025-04-30 11:05:49 +0200

    #421 refactored container autocomplete

--- nuxtend/components/context/data-references/create-dialog/ContainerInput.vue
+++ nuxtend/components/context/data-references/create-dialog/ContainerInput.vue
@@ -1,29 +1,67 @@
 <script setup lang="ts">
 import type { ContainerType } from "@dlr-shepard/backend-client";
+import type { AutoCompleteItem } from "~/components/common/AutocompleteInput.vue";
+import {
+  useContainerSearch,
+  type MyContainerSearchResult,
+} from "~/composables/context/useContainerSearch";
 
 defineProps<{ collectionId: number }>();
 
 const containerId = defineModel<number | null>("containerId", {
   required: true,
 });
+
 const emit = defineEmits<{
   (e: "searchEnded", id: number, type: ContainerType): void;
 }>();
 
-function searchEnded(id: number | null, type: ContainerType | null) {
-  if (!id || !type) return;
-  containerId.value = id;
-  emit("searchEnded", id, type);
+const searchString = ref<string | undefined>(undefined);
+const searchDone = ref<boolean>(false);
+
+const autoCompleteModel = ref<AutoCompleteItem | undefined>(undefined);
+
+const { containerSearchResults, startSearch, isLoading } = useContainerSearch(
+  searchString,
+  () => {
+    searchDone.value = true;
+  },
+);
+
+function mapToSearchResultAutoCompleteItem(
+  searchResult: MyContainerSearchResult,
+): AutoCompleteItem {
+  return {
+    title: `${searchResult.containerName} (ID: ${searchResult.containerId}) (Type: ${searchResult.containerType})`,
+    value: searchResult,
+  };
 }
+
+const onSelect = (selectedItem: AutoCompleteItem | null) => {
+  if (selectedItem?.value) {
+    const container = selectedItem.value as MyContainerSearchResult;
+    containerId.value = container.containerId;
+    emit("searchEnded", container.containerId, container.containerType);
+  }
+};
 </script>
 
 <template>
   <v-row>
     <v-col cols="12">
-      <ContainerAutocomplete
-        input-label="Container Name or ID..."
-        :initial-container-id="containerId"
-        @search-ended="searchEnded"
+      <AutocompleteInput
+        v-model:search-string="searchString"
+        v-model:search-done="searchDone"
+        :model-value="autoCompleteModel"
+        label="Container Name or ID..."
+        density="compact"
+        :is-loading="isLoading"
+        :item-list="
+          containerSearchResults.map(mapToSearchResultAutoCompleteItem)
+        "
+        clearable
+        :start-search="startSearch"
+        @search-ended="onSelect"
       />
     </v-col>
   </v-row>
