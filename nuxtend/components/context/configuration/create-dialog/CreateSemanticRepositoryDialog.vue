943ec78d7871332347a0db9d070581b81bd6a02f
Lukas Werner <lukas.werner@xitaso.com>
2025-05-07 14:14:16 +0000

    #437 implemented  semantic repo creation dialog

---
+++ nuxtend/components/context/configuration/create-dialog/CreateSemanticRepositoryDialog.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import {
+  SemanticRepositoryApi,
+  SemanticRepositoryType,
+} from "@dlr-shepard/backend-client";
+
+const isValid = ref(true);
+const showDialog = defineModel<boolean>("showDialog", {
+  required: true,
+  default: false,
+});
+const semanticRepositoryName = ref<string>("");
+const endpoint = ref<string>("");
+const semanticRepositoryType = ref<SemanticRepositoryType>(
+  SemanticRepositoryType.Sparql,
+);
+const emit = defineEmits(["semantic-repository-created"]);
+
+async function createSemanticRepository() {
+  createApiInstance(SemanticRepositoryApi)
+    .createSemanticRepository({
+      semanticRepository: {
+        name: semanticRepositoryName.value,
+        type: semanticRepositoryType.value,
+        endpoint: endpoint.value,
+      },
+    })
+    .then(response => {
+      emitSuccess(
+        `Successfully created semantic repository "${response.name}"`,
+      );
+      emit("semantic-repository-created");
+      showDialog.value = false;
+    })
+    .catch(error => {
+      handleError(error, "createSemanticRepository");
+    });
+}
+</script>
+
+<template>
+  <FormDialog
+    v-model:show-dialog="showDialog"
+    :max-width="800"
+    title="Create Semantic Repository"
+    :submit-disabled="!isValid"
+    save-button-text="Save"
+    @submit="createSemanticRepository"
+  >
+    <template #form>
+      <v-form ref="form" v-model="isValid">
+        <v-row class="pt-9 pb-1">
+          <v-col>
+            <NameInput v-model:name="semanticRepositoryName" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <SemanticRepositoryTypeSelect
+              v-model:semantic-repository-type="semanticRepositoryType"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <EndpointInput v-model:endpoint="endpoint" />
+          </v-col>
+        </v-row>
+      </v-form>
+    </template>
+  </FormDialog>
+</template>
---
+++ nuxtend/components/context/configuration/create-dialog/EndpointInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const endpoint = defineModel<string>("endpoint", {
+  required: true,
+});
+
+const endpointRules = [
+  (value: unknown) => {
+    if (value) return true;
+    return "Endpoint is required.";
+  },
+];
+</script>
+
+<template>
+  <v-text-field
+    v-model:model-value="endpoint"
+    :rules="endpointRules"
+    label="Endpoint*"
+    variant="outlined"
+    density="compact"
+    require
+    color="primary"
+    hide-details
+  />
+</template>
