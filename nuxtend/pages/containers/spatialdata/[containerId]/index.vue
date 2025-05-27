2df84ca2d24d491d25f66c6b7e908cbe275bbd67
mweslati <mohamed.weslati@xitaso.com>
2025-05-27 14:11:36 +0200

    Small layout adjustment

--- nuxtend/pages/containers/spatialdata/[containerId]/index.vue
+++ nuxtend/pages/containers/spatialdata/[containerId]/index.vue
@@ -17,48 +17,46 @@ onContainerUpdated(() => {
 fetchData();
 </script>
 <template>
-  <div style="max-width: 1200px; margin: auto">
-    <v-container class="pa-0 fill-height" fluid>
-      <v-row v-if="!!container.spatialData.value" no-gutters>
-        <v-col cols="12">
-          <Breadcrumbs
-            :items="[
-              {
-                title: 'Containers',
-                to: containersPath,
-              },
-              {
-                title: container.spatialData.value.name,
-                to: containersPath + urlSegment + containerId,
-              },
-            ]"
-          />
-        </v-col>
-        <v-col cols="12">
-          <v-container class="pa-0" fluid>
-            <v-row no-gutters>
-              <ContainerTitleAndMetadataDisplay
-                :id="container.spatialData.value.id"
-                :name="container.spatialData.value.name"
-                :type-label="'Spatial Data Container'"
-              >
-                <template #buttons>
-                  <EditPermissionsButton
-                    v-if="container.isAllowedToEditPermissions.value"
-                    :shepard-object-accessor="container"
-                  />
-                  <DeleteContainerButton
-                    v-if="container.isAllowedToDelete.value"
-                    :entity-name="container.spatialData.value.name"
-                    @delete="container.delete()"
-                  />
-                </template>
-              </ContainerTitleAndMetadataDisplay>
-            </v-row>
-          </v-container>
-        </v-col>
-      </v-row>
-      <CenteredLoadingSpinner v-else />
-    </v-container>
-  </div>
+  <v-container fluid style="max-width: 1200px; margin: auto">
+    <v-row v-if="!!container.spatialData.value" no-gutters>
+      <v-col cols="12">
+        <Breadcrumbs
+          :items="[
+            {
+              title: 'Containers',
+              to: containersPath,
+            },
+            {
+              title: container.spatialData.value.name,
+              to: containersPath + urlSegment + containerId,
+            },
+          ]"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-container class="pa-0" fluid>
+          <v-row no-gutters>
+            <ContainerTitleAndMetadataDisplay
+              :id="container.spatialData.value.id"
+              :name="container.spatialData.value.name"
+              :type-label="'Spatial Data Container'"
+            >
+              <template #buttons>
+                <EditPermissionsButton
+                  v-if="container.isAllowedToEditPermissions.value"
+                  :shepard-object-accessor="container"
+                />
+                <DeleteContainerButton
+                  v-if="container.isAllowedToDelete.value"
+                  :entity-name="container.spatialData.value.name"
+                  @delete="container.delete()"
+                />
+              </template>
+            </ContainerTitleAndMetadataDisplay>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+    <CenteredLoadingSpinner v-else />
+  </v-container>
 </template>
