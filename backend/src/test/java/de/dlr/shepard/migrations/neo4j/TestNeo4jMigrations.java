4ac880e7ec0ce832238d0131300db4c9f54e045c
Felix Lettowsky <felix.lettowsky@dlr.de>
2026-02-20 16:26:01 +0100

    create test case for V11 migration

--- backend/src/test/java/de/dlr/shepard/migrations/neo4j/TestNeo4jMigrations.java
+++ backend/src/test/java/de/dlr/shepard/migrations/neo4j/TestNeo4jMigrations.java
@@ -89,7 +89,20 @@ public class TestNeo4jMigrations {
 
   private static <T> List<T> match(Node node, Class<T> type) {
     var statement = Cypher.match(node).returning(node).build();
-    var result = query(statement);
+    return queryResults(statement, type);
+  }
+
+  private static <T> List<T> queryResults(Statement statement, Class<T> type) {
+    var cypherQuery = cypherRenderer.render(statement);
+    return queryResults(cypherQuery, type);
+  }
+
+  private static List<Object> queryResults(String cypherQuery) {
+    return queryResults(cypherQuery, Object.class);
+  }
+
+  private static <T> List<T> queryResults(String cypherQuery, Class<T> type) {
+    var result = session.query(cypherQuery, Collections.emptyMap());
     return StreamSupport.stream(result.spliterator(), false)
       .map(Map::values)
       .flatMap(java.util.Collection::stream)
@@ -161,10 +174,115 @@ public class TestNeo4jMigrations {
    * Assert that all referenced timeseries migrated correctly towards individual timeseries.
    */
   @Test
-  public void testV11_ReferencedTimeseriesMigrated() {
+  public void testV11_1_ReferencedTimeseriesMigrated() {
     createReferencedTimeseries();
     runMigrations("V11");
-    fail();
+    var tsNode1 = node("Timeseries")
+      .withProperties(
+        "measurement",
+        Cypher.literalOf("measurement-" + randomElement),
+        "device",
+        Cypher.literalOf("device-" + randomElement),
+        "location",
+        Cypher.literalOf("location-" + randomElement),
+        "symbolicName",
+        Cypher.literalOf("symbolicName-" + randomElement),
+        "field",
+        Cypher.literalOf("field-" + randomElement)
+      )
+      .named("tsNode");
+    var tsNode2 = node("Timeseries")
+      .withProperties(
+        "measurement",
+        Cypher.literalOf("measurement-" + randomElement),
+        "device",
+        Cypher.literalOf("device-" + randomElement),
+        "location",
+        Cypher.literalOf("location-" + randomElement),
+        "symbolicName",
+        Cypher.literalOf("symbolicName-" + randomElement),
+        "field",
+        Cypher.literalOf("field-" + randomElement)
+      )
+      .named("tsNode2");
+    var ref1 = node("TimeseriesReference", "VersionableEntity", "BasicReference", "BasicEntity")
+      .withProperties(
+        "createdAt",
+        Cypher.literalOf(100),
+        "deleted",
+        Cypher.literalOf(false),
+        "end",
+        Cypher.literalOf(2000),
+        "name",
+        Cypher.literalOf("ref-1-" + randomElement),
+        "shepardId",
+        Cypher.literalOf(5),
+        "start",
+        Cypher.literalOf(1000)
+      )
+      .named("ref1");
+    var ref2 = node("TimeseriesReference", "VersionableEntity", "BasicReference", "BasicEntity")
+      .withProperties(
+        "createdAt",
+        Cypher.literalOf(100),
+        "deleted",
+        Cypher.literalOf(false),
+        "end",
+        Cypher.literalOf(2000),
+        "name",
+        Cypher.literalOf("ref-2-" + randomElement),
+        "shepardId",
+        Cypher.literalOf(6),
+        "start",
+        Cypher.literalOf(1000)
+      )
+      .named("ref2");
+    var annotation = node("SemanticAnnotation").withProperties(
+      "propertyName",
+      Cypher.literalOf("prop-on-ts-ref-" + randomElement),
+      "valueName",
+      Cypher.literalOf("value-on-ts-ref-" + randomElement)
+    );
+    var container1 = node("TimeseriesContainer", "BasicEntity", "BasicContainer").withProperties(
+      "createdAt",
+      Cypher.literalOf(200),
+      "deleted",
+      Cypher.literalOf(false),
+      "name",
+      Cypher.literalOf("TimeseriesContainer-1-" + randomElement)
+    );
+    var container2 = node("TimeseriesContainer", "BasicEntity", "BasicContainer").withProperties(
+      "createdAt",
+      Cypher.literalOf(300),
+      "deleted",
+      Cypher.literalOf(false),
+      "name",
+      Cypher.literalOf("TimeseriesContainer-2-" + randomElement)
+    );
+    var result = Cypher.match(
+      ref1.relationshipTo(tsNode1, "has_payload"),
+      ref2.relationshipTo(tsNode2, "has_payload"),
+      ref1.relationshipTo(container1, "is_in_container"),
+      ref2.relationshipTo(container2, "is_in_container"),
+      ref1.relationshipTo(annotation, "has_annotation"),
+      tsNode1.relationshipTo(container1, "is_in_container"),
+      tsNode2.relationshipTo(container2, "is_in_container")
+    )
+      .returning(Cypher.asterisk())
+      .build();
+    var results = queryResults(result, Object.class);
+    assertEquals(1, results.size());
+  }
+
+  /**
+   * Assert that now each timeseries has a relationship to exactly one container.
+   */
+  @Test
+  public void testV11_2_EachTimeseriesHasOneContainer() {
+    var tsWithExactlyOneContainer = queryResults(
+      "match(ts:Timeseries)-[r:is_in_container]->() with ts, count(r) as relcount where relcount = 1 return ts"
+    );
+    assertEquals(2, tsWithExactlyOneContainer.size());
   }
 
   /**
