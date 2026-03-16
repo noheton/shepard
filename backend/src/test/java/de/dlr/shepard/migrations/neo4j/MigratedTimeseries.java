2336991df5c13ca60d1664490de08f980035f10b
Felix Lettowsky <felix.lettowsky@dlr.de>
2026-03-16 13:36:49 +0100

    Build up test structure

---
+++ backend/src/test/java/de/dlr/shepard/migrations/neo4j/MigratedTimeseries.java
@@ -0,0 +1,78 @@
+package de.dlr.shepard.migrations.neo4j;
+
+import de.dlr.shepard.common.neo4j.entities.Annotatable;
+import de.dlr.shepard.common.util.Constants;
+import de.dlr.shepard.common.util.HasId;
+import de.dlr.shepard.context.semantic.entities.SemanticAnnotation;
+import de.dlr.shepard.data.timeseries.model.TimeseriesContainer;
+import de.dlr.shepard.data.timeseries.model.enums.DataPointValueType;
+import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+@NodeEntity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class MigratedTimeseries implements HasId, Annotatable {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @NotBlank
+  @NonNull
+  private String measurement;
+
+  @NotBlank
+  @NonNull
+  private String device;
+
+  @NotBlank
+  @NonNull
+  private String location;
+
+  @NotBlank
+  @NonNull
+  private String symbolicName;
+
+  @NotBlank
+  @NonNull
+  private String field;
+
+  @NotBlank
+  @NonNull
+  private DataPointValueType valueType;
+
+  @NotBlank
+  @NonNull
+  private Long timeseriesId;
+
+  @Relationship(type = Constants.IS_IN_CONTAINER)
+  @NotBlank
+  @NonNull
+  private TimeseriesContainer container;
+
+  @Relationship(type = Constants.HAS_ANNOTATION)
+  @NotBlank
+  @NonNull
+  private List<SemanticAnnotation> annotations = new ArrayList<>();
+
+  @Override
+  public String getUniqueId() {
+    return String.valueOf(timeseriesId);
+  }
+
+  @Override
+  public void addAnnotation(SemanticAnnotation annotation) {
+    annotations.add(annotation);
+  }
+}
--- backend/src/test/java/de/dlr/shepard/migrations/neo4j/TestNeo4jMigrations.java
+++ backend/src/test/java/de/dlr/shepard/migrations/neo4j/TestNeo4jMigrations.java
@@ -1,13 +1,37 @@
 package de.dlr.shepard.migrations.neo4j;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.cypherdsl.core.Cypher.*;
 
+import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.exceptions.CsvValidationException;
 import de.dlr.shepard.common.neo4j.MigrationsRunner;
 import de.dlr.shepard.context.collection.entities.Collection;
+import de.dlr.shepard.data.timeseries.io.TimeseriesWithDataPoints;
+import de.dlr.shepard.data.timeseries.model.Timeseries;
+import de.dlr.shepard.data.timeseries.model.TimeseriesContainer;
+import de.dlr.shepard.data.timeseries.model.TimeseriesDataPoint;
+import de.dlr.shepard.data.timeseries.model.enums.DataPointValueType;
+import jakarta.validation.constraints.NotNull;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -457,5 +481,259 @@ public class TestNeo4jMigrations {
     var container = c.timeseriesContainer();
     var query = Cypher.match(ref.relationshipTo(container, IS_IN_CONTAINER)).returning(ref).build();
     assertEquals(1, q.queryResults(query).size());
+  }
+
+  /**
+   * Get the column name into which a timeseries data point should be saved in timescale.
+   */
+  private static String getDatapointColumn(TimeseriesDataPoint p) {
+    if (p.getValue() instanceof Double) return "double_value";
+    else if (p.getValue() instanceof String) return "string_value";
+    else if (p.getValue() instanceof Boolean) return "boolean_value";
+    else if (p.getValue() instanceof Integer) return "int_value";
+    throw new RuntimeException("Data point " + p + " is of unfitting value!");
+  }
+
+  /**
+   * Helper class to represent a timeseries including its corresponding container id.
+   */
+  @EqualsAndHashCode(callSuper = true)
+  @Data
+  private static class ContaineredTs extends TimeseriesWithDataPoints {
+
+    @NotNull
+    private long containerId;
+
+    public ContaineredTs(long containerId, Timeseries timeseries, List<TimeseriesDataPoint> points) {
+      super(timeseries, points);
+      this.containerId = containerId;
+    }
+  }
+
+  private static ContaineredTs csvEntryToTs(Map<String, String> entry) {
+    return new ContaineredTs(
+      Long.parseLong(entry.get("CONTAINERID")),
+      new Timeseries(
+        entry.get("MEASUREMENT"),
+        entry.get("DEVICE"),
+        entry.get("LOCATION"),
+        entry.get("SYMBOLICNAME"),
+        entry.get("FIELD")
+      ),
+      List.of(new TimeseriesDataPoint(Long.parseLong(entry.get("TIMESTAMP")), strValueToObject(entry.get("VALUE"))))
+    );
+  }
+
+  /**
+   * Convert a string value of a timeseries to an Object of type Integer, Double, Boolean or String.
+   */
+  private static Object strValueToObject(String strValue) {
+    try {
+      return Integer.valueOf(strValue);
+    } catch (NumberFormatException e1) {
+      try {
+        return Double.valueOf(strValue);
+      } catch (NumberFormatException e2) {
+        if ("true".equalsIgnoreCase(strValue) || "false".equalsIgnoreCase(strValue)) return Boolean.valueOf(strValue);
+        else return strValue;
+      }
+    }
+  }
+
+  /**
+   * Determine the value type of a timeseries value
+   * @param value Object that contains the value of the timeseries data point
+   * @return The type corresponding to the value
+   */
+  private static DataPointValueType valueToValueType(Object value) {
+    var strValue = value.toString();
+    try {
+      Integer.valueOf(strValue);
+      return DataPointValueType.Integer;
+    } catch (NumberFormatException e1) {
+      try {
+        Double.valueOf(strValue);
+        return DataPointValueType.Double;
+      } catch (NumberFormatException e2) {
+        if ("true".equalsIgnoreCase(strValue) || "false".equalsIgnoreCase(strValue)) return DataPointValueType.Boolean;
+        else return DataPointValueType.String;
+      }
+    }
+  }
+
+  /**
+   * Read a csv file into a list of maps each mapping header to corresponding value.
+   */
+  private static List<Map<String, String>> readCsvAsMapList(String csvFilePath)
+    throws IOException, CsvValidationException {
+    CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(csvFilePath));
+
+    List<Map<String, String>> rows = new ArrayList<>();
+    Map<String, String> rowMap;
+
+    while ((rowMap = reader.readMap()) != null) {
+      rows.add(rowMap);
+    }
+
+    return rows;
+  }
+
+  List<Long> testingTimeseriesIds;
+
+  /**
+   * Assert that the timeseries from timescale are now present as timeseries nodes in the graph database.
+   */
+  @Test
+  public void testV12_01_tempTimeseriesPresentInGraphDb()
+    throws ClassNotFoundException, IOException, CsvValidationException {
+    prepareV12TimescaleData();
+
+    runMigrations("V12");
+
+    var ts_result_list = q.match(node("Timeseries"));
+    assertEquals(8, ts_result_list.size());
+    assertPresent(testingTimeseriesIds.get(0), 1, "motion", DataPointValueType.Boolean);
+    assertPresent(testingTimeseriesIds.get(1), 2, "motion", DataPointValueType.Boolean);
+    assertPresent(testingTimeseriesIds.get(2), 1, "motion", DataPointValueType.Double);
+    assertPresent(testingTimeseriesIds.get(3), 2, "motion", DataPointValueType.Double);
+    assertPresent(testingTimeseriesIds.get(4), 1, "status", DataPointValueType.String);
+    assertPresent(testingTimeseriesIds.get(5), 2, "status", DataPointValueType.String);
+    assertPresent(testingTimeseriesIds.get(6), 1, "int_level", DataPointValueType.Integer);
+    assertPresent(testingTimeseriesIds.get(7), 2, "int_level", DataPointValueType.Integer);
+  }
+
+  /**
+   * Assert that meta-information does not exist anymore in timescaledb.
+   */
+  @Test
+  public void testV12_02_metadataDeletedInTimeseriesDb() throws SQLException, ClassNotFoundException {
+    try (var connection = createTimeseriesConnection()) {
+      var result = connection
+        .prepareStatement("select tablename from pg_tables where tablename = 'timeseries'")
+        .executeQuery();
+      assertFalse(result.next());
+    }
+  }
+
+  /**
+   * Assert that the values of all timeseries are still intact in timescaledb.
+   */
+  @Test
+  public void testV12_03_timeseriesDatapointsIntact() throws SQLException, ClassNotFoundException {
+    try (var connection = createTimeseriesConnection()) {
+      var tsCount = connection
+        .prepareStatement("select count(timeseries_id) from timeseries_data_points")
+        .executeQuery();
+      tsCount.next();
+      assertEquals(15, tsCount.getInt("count"));
+      var actualTsIds = connection
+        .prepareStatement("select timeseries_id from timeseries_data_points group by timeseries_id")
+        .executeQuery();
+      while (actualTsIds.next()) assertTrue(testingTimeseriesIds.contains(actualTsIds.getLong("timeseries_id")));
+    }
+  }
+
+  private Connection createTimeseriesConnection() throws SQLException, ClassNotFoundException {
+    Class.forName("org.postgresql.Driver");
+    var url = ConfigProvider.getConfig().getValue("quarkus.datasource.jdbc.url", String.class);
+    var user = ConfigProvider.getConfig().getValue("quarkus.datasource.username", String.class);
+    var pass = ConfigProvider.getConfig().getValue("quarkus.datasource.password", String.class);
+    return DriverManager.getConnection(url, user, pass);
+  }
+
+  /**
+   Prepare the timeseries database for the migration of timeseries metadata towards the graph database.
+   */
+  private void prepareV12TimescaleData() throws CsvValidationException, IOException, ClassNotFoundException {
+    Class.forName("org.postgresql.Driver");
+    var url = ConfigProvider.getConfig().getValue("quarkus.datasource.jdbc.url", String.class);
+    var user = ConfigProvider.getConfig().getValue("quarkus.datasource.username", String.class);
+    var pass = ConfigProvider.getConfig().getValue("quarkus.datasource.password", String.class);
+    Flyway flyway = Flyway.configure().dataSource(url, user, pass).load();
+    flyway.migrate();
+
+    var dbEntries = readCsvAsMapList("src/test/resources/timeseries_import_migration_test.csv");
+    var ts_list = dbEntries.stream().map(TestNeo4jMigrations::csvEntryToTs);
+
+    testingTimeseriesIds = ts_list
+      .map(ts -> {
+        try (var connection = createTimeseriesConnection()) {
+          var point = ts.getPoints().getFirst();
+          var sql = Files.readString(Path.of("src/test/resources/insert_timeseries.sql"));
+          var stmt = connection.prepareStatement(sql);
+          stmt.setBigDecimal(1, BigDecimal.valueOf(ts.getContainerId()));
+          stmt.setString(2, ts.getTimeseries().getMeasurement());
+          stmt.setString(3, ts.getTimeseries().getField());
+          stmt.setString(4, ts.getTimeseries().getSymbolicName());
+          stmt.setString(5, ts.getTimeseries().getDevice());
+          stmt.setString(6, ts.getTimeseries().getLocation());
+          stmt.setString(7, valueToValueType(point.getValue()).toString());
+          var resultSet = stmt.executeQuery();
+          var ts_id = resultSet.next() ? Optional.of(resultSet.getInt(1)) : Optional.empty();
+
+          var sql2 = Files.readString(Path.of("src/test/resources/insert_timeseries_data_point.sql"));
+          sql2 = sql2.replace(":column", getDatapointColumn(point));
+          var stmt2 = connection.prepareStatement(sql2);
+          stmt2.setBigDecimal(1, BigDecimal.valueOf(ts.getContainerId()));
+          stmt2.setString(2, ts.getTimeseries().getMeasurement());
+          stmt2.setString(3, ts.getTimeseries().getField());
+          stmt2.setString(4, ts.getTimeseries().getSymbolicName());
+          stmt2.setString(5, ts.getTimeseries().getDevice());
+          stmt2.setString(6, ts.getTimeseries().getLocation());
+          stmt2.setBigDecimal(7, BigDecimal.valueOf(point.getTimestamp()));
+          stmt2.setObject(8, point.getValue());
+          stmt2.executeUpdate();
+          connection.close();
+          return ts_id;
+        } catch (SQLException | IOException | ClassNotFoundException e) {
+          throw new RuntimeException(e);
+        }
+      })
+      .filter(Optional::isPresent)
+      .map(Optional::get)
+      .map(Object::toString)
+      .map(Long::valueOf)
+      .toList();
+  }
+
+  private void assertPresent(long timeseriesId, long containerId, String measurement, DataPointValueType valueType) {
+    var tsExpected = createMigratedTimeseries(containerId, measurement, valueType, timeseriesId);
+    var tsListActual = q.match(
+      node("Timeseries")
+        .withProperties(
+          "timeseriesId",
+          Cypher.literalOf(timeseriesId),
+          "measurement",
+          Cypher.literalOf(measurement),
+          "valueType",
+          Cypher.literalOf(valueType.toString())
+        )
+        .relationshipTo(node("TimeseriesContainer").withProperties("containerId", Cypher.literalOf(containerId)))
+        .getLeft()
+    );
+    assertEquals(1, tsListActual.size());
+    assertEquals(tsExpected, tsListActual.getFirst());
+  }
+
+  /**
+   * Create a migrated timeseries from containerId, measurement and valueType.
+   * device, location, symbolicName and field remain fixed.
+   */
+  private static MigratedTimeseries createMigratedTimeseries(
+    long containerId,
+    String measurement,
+    DataPointValueType valueType,
+    long timeseriesId
+  ) {
+    return new MigratedTimeseries(
+      measurement,
+      "device",
+      "location",
+      "symbolicName",
+      "field",
+      valueType,
+      timeseriesId,
+      new TimeseriesContainer(containerId)
+    );
   }
 }
