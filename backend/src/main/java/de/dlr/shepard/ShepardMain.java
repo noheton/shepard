b93dc0cb9ff7a4a143e722a5ed2a9dedca631432
Felix Lettowsky <felix.lettowsky@dlr.de>
2026-03-19 15:07:31 +0100

    schedule migration order

--- backend/src/main/java/de/dlr/shepard/ShepardMain.java
+++ backend/src/main/java/de/dlr/shepard/ShepardMain.java
@@ -10,6 +10,7 @@ import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.Shutdown;
 import io.quarkus.runtime.Startup;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.annotation.Nullable;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import org.flywaydb.core.Flyway;
@@ -28,16 +29,20 @@ public class ShepardMain implements QuarkusApplication {
 
     migrateTimescale("1.7.0");
 
+    migrateNeo4j("V12");
+
+    Log.info("Run leftover timescale migrations...");
+    flyway.migrate();
+    Log.info("Finished leftover timescale migrations...");
+
+    Log.info("Run leftover Neo4j migrations...");
+    var neoRunner = new MigrationsRunner();
+    neoRunner.waitForConnection();
+    neoRunner.apply();
+    Log.info("Finished leftover Neo4j migrations...");
+
     var pkiHelper = new PKIHelper();
-    var migrationRunner = new MigrationsRunner();
     pkiHelper.init();
-
-    Log.info("Waiting for neo4j database...");
-    migrationRunner.waitForConnection();
-
-    Log.info("Run neo4j database migrations...");
-    migrationRunner.apply();
-    Log.info("Finished neo4j database migrations!");
 
     Log.info("Initialize neo4j databases...");
     neo4j.connect();
@@ -46,8 +51,18 @@ public class ShepardMain implements QuarkusApplication {
 
   private void migrateTimescale(String version) {
     Log.info("Run PostGres migrations until V" + version + "...");
-    Flyway.configure().configuration(flyway.getConfiguration()).target(version).load().migrate();
+    if (version == null) flyway.migrate();
+    else Flyway.configure().configuration(flyway.getConfiguration()).target(version).load().migrate();
     Log.info("Finished PostGres migrations V" + version + "!");
+  }
+
+  private void migrateNeo4j(String version) {
+    var migrationRunner = new MigrationsRunner(version);
+    Log.info("Waiting for neo4j database...");
+    migrationRunner.waitForConnection();
+    Log.info("Run neo4j database migrations until " + version + "...");
+    migrationRunner.apply();
+    Log.info("Finished neo4j database migrations!");
   }
 
   @Override
