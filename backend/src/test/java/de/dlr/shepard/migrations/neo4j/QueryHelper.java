54113df36a42b0f60fa6f1a78715cada476094e1
Felix Lettowsky <felix.lettowsky@dlr.de>
2026-03-20 09:34:28 +0000

    Refactor referenced Timeseries

---
+++ backend/src/test/java/de/dlr/shepard/migrations/neo4j/QueryHelper.java
@@ -0,0 +1,68 @@
+package de.dlr.shepard.migrations.neo4j;
+
+import de.dlr.shepard.common.neo4j.NeoConnector;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+import org.neo4j.cypherdsl.core.Cypher;
+import org.neo4j.cypherdsl.core.Node;
+import org.neo4j.cypherdsl.core.PatternElement;
+import org.neo4j.cypherdsl.core.Statement;
+import org.neo4j.cypherdsl.core.renderer.Renderer;
+import org.neo4j.ogm.model.Result;
+import org.neo4j.ogm.session.Session;
+
+public class QueryHelper {
+
+  private static final Renderer cypherRenderer = Renderer.getDefaultRenderer();
+  private static Session session;
+
+  public QueryHelper() {
+    var conn = NeoConnector.getInstance();
+    conn.connect();
+    session = conn.getNeo4jSession();
+  }
+
+  public Result query(Statement statement) {
+    var cypherQuery = cypherRenderer.render(statement);
+    return session.query(cypherQuery, Collections.emptyMap());
+  }
+
+  public void create(PatternElement... creatable) {
+    var statement = Cypher.create(creatable).build();
+    query(statement);
+  }
+
+  public List<Object> match(Node node) {
+    return match(node, Object.class);
+  }
+
+  public <T> List<T> match(Node node, Class<T> type) {
+    var statement = Cypher.match(node).returning(node).build();
+    return queryResults(statement, type);
+  }
+
+  public <T> List<T> queryResults(Statement statement, Class<T> type) {
+    var cypherQuery = cypherRenderer.render(statement);
+    return queryResults(cypherQuery, type);
+  }
+
+  public List<Object> queryResults(Statement statement) {
+    var cypherQuery = cypherRenderer.render(statement);
+    return queryResults(cypherQuery, Object.class);
+  }
+
+  public List<Object> queryResults(String cypherQuery) {
+    return queryResults(cypherQuery, Object.class);
+  }
+
+  public <T> List<T> queryResults(String cypherQuery, Class<T> type) {
+    var result = session.query(cypherQuery, Collections.emptyMap());
+    return StreamSupport.stream(result.spliterator(), false)
+      .map(Map::values)
+      .flatMap(java.util.Collection::stream)
+      .map(type::cast)
+      .toList();
+  }
+}
